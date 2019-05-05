63663732b28791fb647de3ed816fca3355045dba
Angel <angeljuarez729@gmail.com>
2019-05-05 00:36:32 -0400

    imported react-router-dom so that I could make the routes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Navbar, NavbarBrand, Nav,NavItem } from 'reactstrap';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import App from '../App';
+import Search from './Search';
 
 function Navigation(){
   return(
     <div>
-      <h1>navigation component</h1>
+      <Router>
+        <Navbar>
+          <NavbarBrand href="/"><Link to="/">Stock Market Page</Link></NavbarBrand> 
+          <Nav>
+            <NavItem><Link to="/search/">Search Page</Link></NavItem>
+          </Nav>
+        </Navbar>
+      </Router>
     </div>
   )
 }
