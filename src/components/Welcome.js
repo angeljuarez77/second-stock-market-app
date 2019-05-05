31261388bfeb976e20f0a710e191d13b069a15ee
Angel <angeljuarez729@gmail.com>
2019-05-05 03:01:54 -0400

    added description of website

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,9 @@ import React from 'react';
 function Welcome(){
   return(
     <div>
-      <h1>welcome component</h1>
+      <h1>Welcome</h1>
+      <p>This is a webpage where you could search up stocks for any company you want.</p>
+      <p>Take note that any information in this webpage is not meant to be accurate enough in order to be used in any way besides novelty.</p>
     </div>
   )
 }
