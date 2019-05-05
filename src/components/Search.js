66f3159ccdd2a1be891e9acbc941796d37727f99
Angel <angeljuarez729@gmail.com>
2019-05-05 03:02:08 -0400

    Copy and pasted components from react strap documentation
    am changing the class component to a functional one and logic from using local state to hooks.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButtonDropdown,
+  Input,
+  Button,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+ } from 'reactstrap';
 
-function Search(){
-  return(
-    <div>
-      <h1>search component</h1>
-    </div>
-  )
+export default function Search(){
+  // constructor(props) {
+  //   super(props);
+
+  //   this.toggleDropDown = this.toggleDropDown.bind(this);
+  //   this.toggleSplit = this.toggleSplit.bind(this);
+  //   this.state = {
+  //     dropdownOpen: false,
+  //     splitButtonOpen: false
+  //   };
+  // }
+
+  // toggleDropDown() {
+  //   this.setState({
+  //     dropdownOpen: !this.state.dropdownOpen
+  //   });
+  // }
+
+  // toggleSplit() {
+  //   this.setState({
+  //     splitButtonOpen: !this.state.splitButtonOpen
+  //   });
+  // }
+
+  const [splitButtonOpen, setSplitButtonOpen] = useState(false);
+
+    return (
+      <div>
+        <InputGroup>
+          <InputGroupButtonDropdown addonType="prepend" toggle={() => setSplitButtonOpen(!splitButtonOpen)}>
+            <Button outline>Split Button</Button>
+            <DropdownToggle split outline />
+            <DropdownMenu>
+              <DropdownItem header>Header</DropdownItem>
+              <DropdownItem disabled>Action</DropdownItem>
+              <DropdownItem>Another Action</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Another Action</DropdownItem>
+            </DropdownMenu>
+          </InputGroupButtonDropdown>
+          <Input placeholder="and..." />
+          <InputGroupAddon addonType="append"><Button color="secondary">I'm a button</Button></InputGroupAddon>
+        </InputGroup>
+      </div>
+    );
 }
-
-export default Search;
