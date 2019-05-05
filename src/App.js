e05a52a5cf3d0084b2328175adbe65758f4ee7a8
Angel <angeljuarez729@gmail.com>
2019-05-04 23:59:48 -0400

    created and added navigation bar to app.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Welcome from './components/Welcome';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
+      <Navigation /> 
       <header className="App-header">
         <Welcome />
       </header>
